8eabeac916efc8b4bc124e5e844f033889343c29
yuankun <yuankun@sensetime.com>
2019-12-16 12:55:57 +0800

    fix label_smooth to Loss module; add resnet from torchvision

--- prototype/model/__init__.py
+++ prototype/model/__init__.py
@@ -13,6 +13,10 @@ from .densenet import densenet121, densenet169, densenet201, densenet161  # noqa
 from .hrnet import HRNet  # noqa: F401
 from .mnasnet import mnasnet  # noqa: F401
 from .nas_zoo import mbnas_t29_x0_84, mbnas_t47_x1_00  # noqa: F401
+from .resnet_official import (  # noqa: F401
+    resnet18_official, resnet34_official, resnet50_official, resnet101_official, resnet152_official,
+    resnext50_32x4d, resnext101_32x8d, wide_resnet50_2, wide_resnet101_2
+)
 
 
 def model_entry(config):
