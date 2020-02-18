1bd8ba5c9dd273c091403485bcda74b26c5c3d67
gongruihao <gongruihao@sensetime.com>
2020-02-18 21:33:56 +0800

    feature(model): add resnet-v2 named as preact resnet

--- prototype/model/__init__.py
+++ prototype/model/__init__.py
@@ -1,5 +1,9 @@
 from .mobilenet_v2 import mobilenet_v2  # noqa: F401
 from .resnet import resnet18, resnet34, resnet50, resnet101, resnet152  # noqa: F401
+from .preact_resnet import (  # noqa: F401
+    preact_resnet18, preact_resnet34, preact_resnet50,
+    preact_resnet101, preact_resnet152
+)
 from .efficientnet import (  # noqa: F401
     efficientnet_b0, efficientnet_b1, efficientnet_b2, efficientnet_b3,
     efficientnet_b4, efficientnet_b5, efficientnet_b6, efficientnet_b7
