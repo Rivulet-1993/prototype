1afc9d018f74cccc8f9bb1a28c953507f4757b60
yuankun <yuankun@sensetime.com>
2020-07-20 17:12:38 +0800

    fix config

--- new_config_experiments/resnet18_exp_railc4_pytorch/run.sh
+++ new_config_experiments/resnet18_exp_railc4_pytorch/run.sh
@@ -1,4 +1,5 @@
+LD_LIBRARY_PATH=/mnt/lustre/share/hezheqi/kestrel_python/deps/lib:$LD_LIBRARY_PATH
 PYTHONPATH=$PYTHONPATH:../../ GLOG_vmodule=MemcachedClient=-1 \
 srun --mpi=pmi2 -p $1 -n$2 --gres=gpu:8 --ntasks-per-node=8 --cpus-per-task=5 \
-python -u -m prototype.solver.cls_solver --config config.yaml \
+python -u -m prototype.solver.cls_solver --config config_train.yaml \
  #--recover=checkpoints/ckpt_1000.pth.tar \
