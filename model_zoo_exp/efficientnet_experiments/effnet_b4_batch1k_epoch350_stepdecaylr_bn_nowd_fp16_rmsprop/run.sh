da9bddd158b17bb29481124966e91688b3b13b09
yuankun <yuankun@sensetime.com>
2020-07-28 21:51:44 +0800

    refactor(config): modify partial config part-1

---
+++ model_zoo_exp/efficientnet_experiments/effnet_b4_batch1k_epoch350_stepdecaylr_bn_nowd_fp16_rmsprop/run.sh
@@ -0,0 +1,3 @@
+PYTHONPATH=$PYTHONPATH:../../../ GLOG_vmodule=MemcachedClient=-1 \
+srun --mpi=pmi2 -p $1 -n32 --gres=gpu:8 --ntasks-per-node=8 --cpus-per-task=5 \
+python -u -m prototype.solver.cls_solver --config config.yaml  # --evaluate
